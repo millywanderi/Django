c4208cfafe6e7073f8d89b0cb973ff1266848266
millywanderi <wanderinyambura5@gmail.com>
2023-09-01 21:33:54 +0300

    Creating new post features

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -133,10 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")] # for static files, html, css, js.
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media") # for images
 
-MEDIA_ROUTE = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
